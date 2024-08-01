d457708f86b7210005724519e1614d5fbcbe48be
kayigmb <kayigm1@gmail.com>
2024-08-01 11:58:42 +0200

    network1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"comment"
+import urllib.request
+
+if __name__ == "__main__":
+    y = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(y) as re:
+        data = re.read()
+        print("Body response:")
+        print(f"\t- type: {type(data)}")
+        print(f"\t- content: {data}")
+        print(f"\t- utf8 content: {data.decode("utf-8")}")
